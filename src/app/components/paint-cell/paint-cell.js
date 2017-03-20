2a83faaf16f67a2cfb7e238c5a53af4aa714ec38
karim <karim@MacBook-Pro-di-karim.local>
2017-03-20 01:13:48 +0100

    overlay cell

--- src/app/components/paint-cell/paint-cell.js
+++ src/app/components/paint-cell/paint-cell.js
@@ -9,13 +9,13 @@
     controllerAs: 'paint'
   });
 
-  paintCellCtrl.$inject = ['$element', '$compile', 'Game', '$rootScope', '$timeout'];
+  paintCellCtrl.$inject = ['$element', '$compile', 'Game', '$rootScope', '$timeout', '$scope'];
 
-  function paintCellCtrl($element, $compile, Game, $rootScope, $timeout){
+  function paintCellCtrl($element, $compile, Game, $rootScope, $timeout, $scope){
 
     var previousVal = this.cell;
-
-    this.$doCheck = function(){
+    var self = this;
+    self.$doCheck = function(){
       if(this.cell !== previousVal){
         if(this.cell === Game.TEAMS.X){
           $element.css('background-image', 'url('+Game.IMAGES.X+')');
@@ -37,6 +37,33 @@
       },200)
 
     })
+
+
+    self.$postLink = function(){
+
+      var overlayHtml =
+      '<div class="paintCell--overlay">'+
+        '<div style="text-align: center; position: relative; width: 100%; height: 100%;">'+
+          '<img class="paintCell--overlay--team" alt="no image" ng-src="{{paint.imgPath}}"/>'+
+        '</div>'+
+      '</div>';
+
+      var overlayEl = angular.element(overlayHtml);
+
+      $element.on('mouseenter', function(){
+        if(!Game.lockBoard && self.cell === Game.TEAMS.EMPTY){
+          self.imgPath = Game.IMAGES[self.game.team];
+          $element.append($compile(overlayEl)($scope));
+        }
+      })
+
+      $element.on('mouseleave', function(){
+        if(!Game.lockBoard){
+          overlayEl.remove();
+        }
+      })
+
+    }
 
   }
 }())
